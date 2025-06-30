a21e60cbe3f3a66b02ba652054f3c4a76e33aedd
jagasia <46959568+jagasia@users.noreply.github.com>
2025-06-30 10:18:57 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
 public class Hello
 {
 	public static void main(String []args){
-		System.out.println("Hello Jag...");
-		System.out.println("now changes from git");
-	System.out.println("After creating web hook in github....");
-	System.out.println("ONe more push");
+		System.out.println("Completely modified by jag now during harman training.");
 	}
 }
